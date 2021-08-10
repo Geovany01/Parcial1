46f3cda9a4a7912be6cdeeb8278b2f5a5bf81fec
Geovany01 <operezv3@miumg.edu.gt>
2021-08-09 21:17:22 -0600

    first comit

---
+++ src/modelo/VehiculoAereo.java
@@ -0,0 +1,39 @@
+
+package modelo;
+
+public class VehiculoAereo extends Vehiculo {
+    private String cantRuedasTrenAterrizaje;
+    
+    public VehiculoAereo() {
+        
+    }
+
+    public VehiculoAereo(String cantRuedasTrenAterrizaje, String marca, String modelo, String color, String matrícula, double precio) {
+        super(marca, modelo, color, matrícula, precio);
+        this.cantRuedasTrenAterrizaje = cantRuedasTrenAterrizaje;
+    }
+
+    public String cantRuedasTrenAterrizaje() {
+        return cantRuedasTrenAterrizaje;
+    }
+
+    public void setCantRuedas(String cantRuedasTrenAterrizaje) {
+        this.cantRuedasTrenAterrizaje = cantRuedasTrenAterrizaje;
+    }
+    
+    protected void girarDerecha() {
+        
+    }
+    
+    protected void girarIzqierda() {
+        
+    }
+    
+    protected void arriba() {
+        
+    }
+    
+    protected void abajo() {
+        
+    }
+}
